d751d962d7a39e7580359d3c6195c1e5c3c6acd7
Jessica <jess.newman19@gmail.com>
2022-05-31 17:06:19 -0400

    added routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,7 +37,7 @@ puts "Creating classes..."
 
 intensity = ["C2", "C1", "B2", "B1", "A2", "A1"]
 type = ["Vinyasa Yoga", "Hatha Yoga", "Iyengar Yoga", "Ashtanga Yoga", "Kundalini Yoga", "Bikram Yoga", "Yin Yoga", "Restorative Yoga", "Prenatal Yoga", "Anusara Yoga", "Jivamukti Yoga"]
-schedule_time = ["6:30am - 7:30am", "7:45am - 8:45am", "9:00am - 10:00am", "10:15am - 11:15am", "11:30am - 12:30pm", "12:45pm - 1:45pm", "2:00pm - 3:00pm", "3:15pm - 4:15pm", "4:30pm - 5:30pm", "5:45pm - 6:45pm", "7:00pm - 8:00pm"]
+class_length = ["45 minutes", "1 hour"]
 
 50.times do |i|
     YogaClass.create(
@@ -46,7 +46,8 @@ schedule_time = ["6:30am - 7:30am", "7:45am - 8:45am", "9:00am - 10:00am", "10:1
         yoga_type: type.sample,
         intensity: intensity.sample,
         schedule_date: Faker::Date.between(from: '2022-06-01', to: '2022-09-01'),
-        schedule_time: schedule_time.sample
+        schedule_start_time: rand(6..20).to_s + ":00",
+        class_length: class_length.sample
     )
 end
 
