8f4d3546c9a58175f0395fe6b973964930ddfb38
Jessica <jess.newman19@gmail.com>
2022-06-01 18:19:15 -0400

    updated student seed data

--- db/migrate/20220531153141_create_students.rb
+++ db/migrate/20220531153141_create_students.rb
@@ -3,7 +3,7 @@ class CreateStudents < ActiveRecord::Migration[6.1]
     create_table :students do |t| 
       t.string :first_name 
       t.string :last_name 
-      t.integer :phone_number 
+      t.string :phone_number 
       t.string :email 
       t.boolean :in_training
       t.string :home_location 
